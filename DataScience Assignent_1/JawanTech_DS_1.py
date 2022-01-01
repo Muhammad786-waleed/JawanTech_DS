ba2184eae2522675517fae492486c7206334a022
Muhammad786-waleed <84074106+Muhammad786-waleed@users.noreply.github.com>
2022-01-01 23:19:05 +0500

    Add files via upload

---
+++ DataScience Assignent_1/JawanTech_DS_1.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("Question 1")
+print("\n")
+print("Twinkle, twinkle, little star, \n\tHow I wonder what you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\tHow I wonder what you are!")
+
+
+# In[2]:
+
+
+print("Question 2")
+print("\n")
+import sys
+print(sys.version)
+
+
+# In[3]:
+
+
+print("Question 3")
+print("\n")
+from datetime import date
+today = date.today()
+print("Today's date:", today)
+
+
+# In[9]:
+
+
+print("Question 4")
+print("\n")
+
+from math import pi
+radius = float(input("Enter The Radius Of The Circle :\t "))
+print("Area Is: " + str(pi * radius**2))
+
+
+# In[10]:
+
+
+print("Question 5")
+print("\n")
+
+firstName = input("Input your First Name : ")
+lastName = input("Input your Last Name : ")
+print ("Hello " + lastName + " " + firstName)
+
+
+# In[11]:
+
+
+print("Question 6")
+print("\n")
+
+num1 = int(input("Enter Your First Number: "))
+num2 = int(input("enter Your Second Number: "))
+sum = num1 + num2
+print("Your Sum is:", sum)
+
+
+# In[ ]:
+
+
+
+
